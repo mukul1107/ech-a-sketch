780180d0b78361e09d438c60fefb86b2e4206268
mukul1107 <mukulsingh3078@gmail.com>
2023-10-11 19:11:49 +0530

    Added user choice functionalty

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const root = document.querySelector("#root");
 const btn = document.querySelector("#root > button");
 const heading = document.querySelector("#dis")
+const main = document.querySelector(".main");
+
 let isTouching = false; // To track touch interaction
+let userInput = getInput();
+if(userInput > 100){
+    console.log("Size should be less than 100.")
+    getInput();
+}
+
+
+function getInput(){
+    return parseInt(prompt("Enter the size of grid: "))
+}
 
 function changeColor(sqr) {
     sqr.style.backgroundColor = "black";
@@ -23,10 +35,12 @@ function handleTouchMove(event) {
     }
 }
 
-function generateGrid(num = 16, className = "active") {
-    for (let i = 0; i < num; i++) {
-        for (let j = 0; j < num; j++) {
+function generateGrid( className = "active") {
+    for (let i = 0; i < userInput; i++) {
+        for (let j = 0; j < userInput; j++) {
             const sqr = document.createElement("div");
+            main.style.cssText = `grid-template-columns: repeat(${userInput}, minmax(30px, 1fr))`
+            
             sqr.classList.add("squares");
             sqr.classList.add(className);
 
